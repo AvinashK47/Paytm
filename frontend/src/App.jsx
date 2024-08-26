e41f1e67a0fd7674c30c4c1bb919138ae4869a31
Avinash <avinash.kushwaha1501@gmail.com>
2024-08-26 23:28:33 +0530

    updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
 
-import {Signin,Signup,Dashboard,SendMoney} from './pages';
+import {SendMoney} from './pages/SendMoney';
+import { Signup } from './pages/Signup';
+import { Signin } from './pages/Signin';
+import { Dashboard } from './pages/Dashboard';
 
 function App() {
 
@@ -8,8 +11,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path = "/signup" element = {<Signup />} />
-          <Route path = "/signin" element = {<Signin />} />
+          <Route path = "/signup" element = {<Signup/>} />
+          <Route path = "/signin" element = {<Signin/>} />
           <Route path = "/dashboard" element = {<Dashboard/>} />
           <Route path = "/send" element = {<SendMoney/>} />
         </Routes>
--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./Button";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const Users = ()=>{
-    const [users , setUsers] = useState([{
-        "firstname" : "Avinash",
-        "lastname" : "Kushwaha",
-        _id : 1
-    }]);
+    const [users , setUsers] = useState([]);
+    const [filter , setFilter] = useState("");
 
+    useEffect(()=>{
+        axios.get("http://localhost/3000/api/v1/user/bulk?filter=" + filter)
+        .then(response=>{
+            setUsers(response.data.user)
+        })
+    },[filter])
     return <>
             <div className="font-bold mt-6 text-lg" >
                 Users
             </div>
             <div className="my-2" >
-                <input type = "text" placeholder = "Search Users ... " className="w-full px-2 py-1 border rounded border-slate-200"></input>
+                <input onChange={(e)=>{setFilter(e.target.value)}} type = "text" placeholder = "Search Users ... " className="w-full px-2 py-1 border rounded border-slate-200"></input>
             </div>
             <div>
                 {users.map( user => <User user ={user} />)}
@@ -21,7 +26,10 @@ export const Users = ()=>{
     </>
 }
 
-function User({user}){
+function User(user){
+    
+    const navigate = useNavigate();
+
     return <div className="flex justify-between" >
         <div className="flex" >
             <div className="rounded full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2 " >
@@ -36,7 +44,9 @@ function User({user}){
             </div>
         </div>
         <div className="flex flex-col justify-center h-full" >
-            <Button label = {"Send Money"} />
+            <Button onClick = {(e) => {
+                navigate("/send?id=" + user._id + "&name="+user.firstname)
+            }} label = {"Send"} />
         </div>
     </div>
 }
